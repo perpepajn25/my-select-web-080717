0e8c4c93c2c20ce8b0ae18695369ed2c4330a384
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-04 11:02:54 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+    i=0
+    true_yields = []
+    while i < collection.length
+        yield(collection[i])
+        if yield(collection[i]) == true
+            true_yields.push(collection[i])
+        end
+        i+=1
+    end
+    return true_yields
 end
